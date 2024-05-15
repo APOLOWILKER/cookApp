556441fa570fd8fa4038f6150bf9763f326737b3
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-15 18:18:19 -0300

    feat: finish ingredients list and card

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,7 +1,21 @@
-import { Text, View } from "react-native";
+import { IngredientCard } from "@/components/IngredientCard";
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 import { styles } from "./styles";
 
 export default function Index() {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  function handleToggleSelected(value: string){
+    if(selected.includes(value)){
+      return setSelected((state) => state.filter((item) => item !== value))
+    }
+
+    setSelected((state) => [...state, value])
+    console.log(selected);
+    
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
@@ -12,6 +26,21 @@ export default function Index() {
       <Text style={styles.message}>
         Descubra receitas baseadas nos produtos que você escolheu.
       </Text>
+
+      <ScrollView 
+    contentContainerStyle={styles.ingredients}
+    showsHorizontalScrollIndicator={false}
+  >
+    {Array.from({ length: 100 }).map((item, index) => (
+      <IngredientCard 
+        key={index} 
+        name="maçã" 
+        image="" 
+        selected={selected.includes(String(index))}
+        onPress={() => handleToggleSelected(String(index))}
+      />
+    ))}
+   </ScrollView>
     </View>
   )
 }
